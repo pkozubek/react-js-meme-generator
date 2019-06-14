4d4e746cbd7d820bfbb83f7cad99325e75de3095
kozi <pkozubek92@gmail.com>
2019-06-14 21:35:37 +0200

    Updates

---
+++ precache-manifest.c0da7ae3e8ee7c96a56c526672efba1e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "11cac8cfa4f32943416da581cb09e0c3",
+    "url": "/react-js-meme-generator/static/media/phone.11cac8cf.jpg"
+  },
+  {
+    "revision": "ef35a6488035df33eb93",
+    "url": "/react-js-meme-generator/static/css/main.1114c080.chunk.css"
+  },
+  {
+    "revision": "1e6af568306b856f680c",
+    "url": "/react-js-meme-generator/static/js/runtime~main.294ea5ab.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-js-meme-generator/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "2fcdc3d4f87348ef98ba",
+    "url": "/react-js-meme-generator/static/js/2.6211fd9f.chunk.js"
+  },
+  {
+    "revision": "1581f5fab64e4a1684f03ef3c969b134",
+    "url": "/react-js-meme-generator/static/media/sad.1581f5fa.jpg"
+  },
+  {
+    "revision": "452b990b966094b7bbcf255c61cae604",
+    "url": "/react-js-meme-generator/static/media/fry.452b990b.jpg"
+  },
+  {
+    "revision": "4e18f32875a58e497f10b353b13c8415",
+    "url": "/react-js-meme-generator/static/media/jobs.4e18f328.jpg"
+  },
+  {
+    "revision": "ef35a6488035df33eb93",
+    "url": "/react-js-meme-generator/static/js/main.67c9a472.chunk.js"
+  },
+  {
+    "revision": "f242473074cfb69336b55fb1d5c3fc37",
+    "url": "/react-js-meme-generator/static/media/wonka.f2424730.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-js-meme-generator/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-js-meme-generator/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-js-meme-generator/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-js-meme-generator/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "2fcdc3d4f87348ef98ba",
+    "url": "/react-js-meme-generator/static/css/2.dc79e245.chunk.css"
+  },
+  {
+    "revision": "2bc84041fe3a9a5bc27405bda722b1fb",
+    "url": "/react-js-meme-generator/index.html"
+  }
+];
